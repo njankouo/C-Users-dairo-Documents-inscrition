19fa78ca8bf567ba4b547fe8e4fc14f6c1093c81
dairou njankouo <dairounjankouo201@gmail.com>
2020-12-30 17:16:33 +0100

    datatale

---
+++ etudiant.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Liste des clients</title>
+
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
+	</head>
+	<body>
+		<section id="menu">
+			<div id="title">
+				Gestion des etudiants
+			</div>
+
+			<div id="tools">
+				<a href="form_ajouter.php">
+					Ajouter
+				</a>
+			</div>
+		</section>
+
+
+
+		
+
+		<table border="1" cellspacing="0" cellpadding="12" id="tableau">
+			<tr>
+				<th>N°</th>
+				<th>MATRICULE</th>
+				<th>NOM</th>
+				<th>PRENOM</th>
+				<th>DATE NAISSANCE</th>
+				<th>LIEU</th>
+				<th>TELEPHONE</th>
+				<th>EMAIL</th>
+
+				<th>MODIFIER</th>
+				<th>SUPPRIMER</th>
+			</tr>
+			
+		
+			<tr>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+
+				<td>
+					<a href="">
+						<button>Modifier</button>
+					</a>
+				</td>
+
+				<td>
+					<a href="">
+						<button>Supprimer</button>
+					</a>
+				</td>
+			</tr>
+		
+		</table>
+
+
+
+
+	</body>
+</html>
